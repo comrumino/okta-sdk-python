5c40dce4309dbeca8735d4677eacb776f0e9ed33
James Stronz <james.stronz@resmed.com>
2025-03-12 12:54:27 -0500

    added test for next page when includeResponse is True and data_type is not None

--- tests/unit/test_api_response.py
+++ tests/unit/test_api_response.py
@@ -1,5 +1,6 @@
 import tests.mocks as mocks
 import pytest
+import okta.models as models
 from okta.client import Client
 from okta.request_executor import RequestExecutor
 
@@ -42,6 +43,35 @@ async def test_response_pagination_with_next(monkeypatch):
     assert not result.has_next()
     with pytest.raises(StopAsyncIteration):
         await result.next()
+
+
+@ pytest.mark.asyncio
+async def test_response_pagination_with_next_include_response(monkeypatch):
+    ssws_client = Client({
+        "orgUrl": ORG_URL,
+        "token": API_TOKEN
+    })
+
+    req, error = await ssws_client.get_request_executor()\
+        .create_request("GET",
+                        GET_USERS_CALL + API_LIMIT,
+                        {},
+                        {})
+
+    monkeypatch.setattr(RequestExecutor, 'fire_request',
+                        mocks.mock_GET_HTTP_Client_response_valid_with_next)
+
+    result, error = await ssws_client.get_request_executor().execute(req, models.Group)
+
+    assert result.get_body() is not None
+    assert result.has_next()
+    assert result._next.startswith(GET_USERS_CALL)
+    monkeypatch.setattr(RequestExecutor, 'fire_request',
+                        mocks.mock_GET_HTTP_Client_response_valid)
+    res, err, resp = await result.next(includeResponse=True)
+    assert isinstance(res[0], models.Group)
+    assert err is None
+    assert resp._type is models.Group
 
 
 @ pytest.mark.asyncio
